657c0f59d5dcd720288f6a784ceb2591506776a4
HammadAkbar1623 <mohammadhammadakbar@gmail.com>
2025-03-17 06:25:43 +0500

    Created Mongoose model for User

---
+++ src/Models/User.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new Schema(
+  {
+    Username: { type: String, required: true, unique: true },
+    Name: { type: String, required: true },
+    Email: { type: String, required: true, unique: true },
+    Password: { type: String, required: true },
+    isVerified: { type: Boolean, default: false },
+    Bio: { type: String },
+    ProfilePic: { type: String },
+    Hashtags: { type: [String] },
+    DeviceToken: { type: String }, // For FCM notifications (optional)
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("Password")) {
+    return next();
+  }
+
+  this.Password = await bcrypt.hash(this.Password, 10);
+  next();
+});
+
+// Checking password
+UserSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.Password);
+};
+
+const User = mongoose.model("User", UserSchema);
+export default User;
