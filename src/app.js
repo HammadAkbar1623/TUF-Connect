41de51df2e3df0597b702ac9bbf098a12613874d
HammadAkbar1623 <mohammadhammadakbar@gmail.com>
2025-04-02 08:01:27 +0500

    Implemented CORS

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import UserRouter from './Routes/User.routes.js'
 const app = express();
-
+app.use(cors());
 app.use(express.json())
 
 app.use("/api/v1/users", UserRouter);
