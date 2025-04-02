6119c03342d7d1dd98e1db016e932fd4e40a0167
HammadAkbar1623 <mohammadhammadakbar@gmail.com>
2025-04-02 08:05:05 +0500

    Added new controller routes

--- src/Routes/User.routes.js
+++ src/Routes/User.routes.js
@@ -7,15 +7,20 @@ import { authenticateUser } from '../Middlewares/auth.middleware.js';
 import { loginUser } from '../Controllers/user.controller.js';
 import { logOutUser } from '../Controllers/user.controller.js';
 import { ChangeCurrentPassword } from '../Controllers/user.controller.js';
-
+import { showAllPosts } from '../Controllers/post.controller.js';
+import { getUserProfile } from '../Controllers/user.controller.js';
+import { deletePost } from '../Controllers/post.controller.js';
 
 const router = Router();
 router.post("/register", registerUser);
 router.post("/verifyOtp",  VerifyOtp);
 router.post("/completeProfile", authenticateUser, CompleteProfile);
 router.post("/createPost", authenticateUser, createPost);
+router.get("/showPosts", authenticateUser, showAllPosts);
+router.delete("/deletePost/:id", authenticateUser, deletePost);
 router.post("/login", loginUser);
-router.post("/logout", authenticateUser, logOutUser);
+router.get('/profile', authenticateUser, getUserProfile);
+router.delete("/logout", authenticateUser, logOutUser);
 router.post("/changePassword", authenticateUser, ChangeCurrentPassword);
 
 export default router;
