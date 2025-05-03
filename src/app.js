bbaaff252c1cf723a8f10f0c1d39e7a1c4015559
HammadAkbar1623 <mohammadhammadakbar@gmail.com>
2025-05-03 13:47:37 +0500

    gonna check on my mobile

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:8081',
+    origin: '*',
     credentials: true,
     methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS']
   }));
